1478f07f42feab2ae137147be5806944718bf8f8
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-11 15:15:14 +0700

    Define state splash page

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,6 +1,10 @@
 import 'package:e_commerce/core/assets/app_vectors.dart';
 import 'package:e_commerce/core/configs/theme/app_colors.dart';
+import 'package:e_commerce/presentation/auth/pages/signin.dart';
+import 'package:e_commerce/presentation/splash/bloc/splash_cubit.dart';
+import 'package:e_commerce/presentation/splash/bloc/splash_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SplashPage extends StatelessWidget {
@@ -8,9 +12,21 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primary,
-      body: Center(child: SvgPicture.asset(AppVectors.appLogo)),
+    return BlocListener<SplashCubit, SplashState>(
+      listener:
+          (context, state) => {
+            if (state is UnAuthenticated)
+              {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => SigninPage()),
+                ),
+              },
+          },
+      child: Scaffold(
+        backgroundColor: AppColors.primary,
+        body: Center(child: SvgPicture.asset(AppVectors.appLogo)),
+      ),
     );
   }
 }
