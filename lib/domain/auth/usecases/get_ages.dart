b29b810d89b32cf5d602b096c90a77e79e9134b3
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-17 17:08:05 +0700

    Get ages usecase

---
+++ lib/domain/auth/usecases/get_ages.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/usecase/usecase.dart';
+import 'package:e_commerce/domain/auth/repository/auth.dart';
+import 'package:e_commerce/service_locator.dart';
+
+class GetAgesUseCase implements UseCase<Either, dynamic> {
+  @override
+  Future<Either> call({dynamic params}) async {
+    return await sl<AuthRepository>().getAges();
+  }
+}
