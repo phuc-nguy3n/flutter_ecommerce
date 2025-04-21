635f416e167e4118c3be4f74453e1fd1f43e8895
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-21 15:15:39 +0700

    Email forgot password

--- lib/presentation/auth/pages/forgot_password.dart
+++ lib/presentation/auth/pages/forgot_password.dart
@@ -1,26 +1,51 @@
+import 'package:e_commerce/common/bloc/button/button_state.dart';
+import 'package:e_commerce/common/bloc/button/button_state_cubit.dart';
+import 'package:e_commerce/common/helper/navigator/app_navigator.dart';
 import 'package:e_commerce/common/widgets/appbar/app_bar.dart';
-import 'package:e_commerce/common/widgets/button/basic_app_button.dart';
-import 'package:flutter/gestures.dart';
+import 'package:e_commerce/common/widgets/button/basic_reactive_button.dart';
+import 'package:e_commerce/domain/auth/usecases/send_password_reset_email.dart';
+import 'package:e_commerce/presentation/auth/pages/password_reset_email.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ForgotPassword extends StatelessWidget {
-  const ForgotPassword({super.key});
+  ForgotPassword({super.key});
+
+  final TextEditingController _emailCon = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BasicAppbar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _forgotPasswordText(context),
-            const SizedBox(height: 20),
-            _emailTextField(context),
-            const SizedBox(height: 20),
-            _continueButton(),
-          ],
+      body: BlocProvider(
+        create: (context) => ButtonStateCubit(),
+        child: BlocListener<ButtonStateCubit, ButtonState>(
+          listener: (context, state) {
+            if (state is ButtonFailureState) {
+              var snackbar = SnackBar(
+                content: Text(state.errorMessage),
+                behavior: SnackBarBehavior.floating,
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackbar);
+            }
+
+            if (state is ButtonSuccessState) {
+              AppNavigator.push(context, const PasswordResetEmailPage());
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _forgotPasswordText(context),
+                const SizedBox(height: 20),
+                _emailTextField(context),
+                const SizedBox(height: 20),
+                _continueButton(),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -35,6 +60,7 @@ class ForgotPassword extends StatelessWidget {
 
   Widget _emailTextField(BuildContext context) {
     return TextField(
+      controller: _emailCon,
       decoration: InputDecoration(
         hintText: 'Enter Email',
         border: OutlineInputBorder(),
@@ -43,11 +69,18 @@ class ForgotPassword extends StatelessWidget {
   }
 
   Widget _continueButton() {
-    return BasicAppButton(
-      onPressed: () {},
-      title: 'Continue',
-      height: 50,
-      width: double.infinity,
+    return Builder(
+      builder: (context) {
+        return BasicReactiveButton(
+          onPressed: () {
+            context.read<ButtonStateCubit>().execute(
+              usecase: SendPasswordResetEmailUseCase(),
+              params: _emailCon.text,
+            );
+          },
+          title: 'Continue',
+        );
+      },
     );
   }
 }
