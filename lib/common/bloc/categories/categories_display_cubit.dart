02cda9a239134170e9eb636e6a1a726e7d00c9af
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-22 16:14:46 +0700

    Set state categories

---
+++ lib/common/bloc/categories/categories_display_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:e_commerce/common/bloc/categories/categories_display_state.dart';
+import 'package:e_commerce/domain/category/usecases/get_categories.dart';
+import 'package:e_commerce/service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CategoriesDisplayCubit extends Cubit<CategoriesDisplayState> {
+  CategoriesDisplayCubit() : super(CategoriesLoading());
+
+  void displayCategories() async {
+    var returnedData = await sl<GetCategoriesUseCase>().call();
+    returnedData.fold(
+      (error) {
+        emit(LoadCategoriesFailure());
+      },
+      (data) {
+        emit(CategoriesLoaded(categories: data));
+      },
+    );
+  }
+}
