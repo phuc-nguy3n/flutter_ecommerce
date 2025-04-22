a91f596e5c790f8a6f543c8a5fbfaaef285bba94
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-22 16:17:48 +0700

    Categories widget

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/presentation/home/widget/categories.dart';
 import 'package:e_commerce/presentation/home/widget/header.dart';
 import 'package:e_commerce/presentation/home/widget/search_field.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,13 @@ class HomePage extends StatelessWidget {
     return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [Header(), SizedBox(height: 24), SearchField()],
+          children: [
+            Header(),
+            SizedBox(height: 24),
+            SearchField(),
+            SizedBox(height: 24),
+            Categories(),
+          ],
         ),
       ),
     );
