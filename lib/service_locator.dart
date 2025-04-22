c29f3be78939ca811345ddcedefadbaead8c0ed1
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-22 16:18:09 +0700

    Categories service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce/data/auth/repository/auth_repository_impl.dart';
 import 'package:e_commerce/data/auth/source/auth_firebase_service.dart';
+import 'package:e_commerce/data/category/repository/category.dart';
+import 'package:e_commerce/data/category/source/category_firebase_service.dart';
 import 'package:e_commerce/domain/auth/repository/auth.dart';
 import 'package:e_commerce/domain/auth/usecases/get_ages.dart';
 import 'package:e_commerce/domain/auth/usecases/get_user.dart';
@@ -7,6 +9,8 @@ import 'package:e_commerce/domain/auth/usecases/is_logged_in.dart';
 import 'package:e_commerce/domain/auth/usecases/send_password_reset_email.dart';
 import 'package:e_commerce/domain/auth/usecases/siginup.dart';
 import 'package:e_commerce/domain/auth/usecases/signin.dart';
+import 'package:e_commerce/domain/category/repository/category.dart';
+import 'package:e_commerce/domain/category/usecases/get_categories.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -15,8 +19,12 @@ Future<void> initializeDependencies() async {
   // Services - Giao tiếp tầng thấp (API, Firebase...)	T
   sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
 
+  sl.registerSingleton<CategoryFirebaseService>(CategoryFirebaseServiceImpl());
+
   // Repositories - Cầu nối giữa UseCase và Service
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+
+  sl.registerSingleton<CategoryRepository>(CategoryRepositoryImpl());
 
   // Usecases - Logic nghiệp vụ
   sl.registerSingleton<SignupUseCase>(SignupUseCase());
@@ -32,4 +40,6 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<IsLoggedInUseCase>(IsLoggedInUseCase());
 
   sl.registerSingleton<GetUserUseCase>(GetUserUseCase());
+
+  sl.registerSingleton<GetCategoriesUseCase>(GetCategoriesUseCase());
 }
