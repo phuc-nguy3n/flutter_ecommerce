54e2577fa47518f7a00ae210ec64607782a477ad
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-26 21:23:07 +0700

    Execute Add To Cart

--- lib/presentation/product_detail/widgets/add_to_bag.dart
+++ lib/presentation/product_detail/widgets/add_to_bag.dart
@@ -1,7 +1,12 @@
+import 'package:e_commerce/common/bloc/button/button_state_cubit.dart';
 import 'package:e_commerce/common/helper/product/product_price.dart';
-import 'package:e_commerce/common/widgets/button/basic_app_button.dart';
+import 'package:e_commerce/common/widgets/button/basic_reactive_button.dart';
+import 'package:e_commerce/data/order/models/add_to_cart_req.dart';
+import 'package:e_commerce/domain/order/usecases/add_to_cart.dart';
 import 'package:e_commerce/domain/product/entities/product.dart';
+import 'package:e_commerce/presentation/product_detail/bloc/product_color_selection_cubit.dart';
 import 'package:e_commerce/presentation/product_detail/bloc/product_quantity_cubit.dart';
+import 'package:e_commerce/presentation/product_detail/bloc/product_size_selection_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,8 +18,33 @@ class AddToBag extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: BasicAppButton(
-        onPressed: () {},
+      child: BasicReactiveButton(
+        onPressed: () {
+          context.read<ButtonStateCubit>().execute(
+            usecase: AddToCartUseCase(),
+            params: AddToCartReq(
+              productId: productEntity.productId,
+              productTitle: productEntity.title,
+              productQuantity: context.read<ProductQuantityCubit>().state,
+              productColor:
+                  productEntity
+                      .colors[context
+                          .read<ProductColorSelectionCubit>()
+                          .selectedIndex]
+                      .title,
+              productSize:
+                  productEntity.sizes[context
+                      .read<ProductSizeSelectionCubit>()
+                      .selectedIndex],
+              productPrice: productEntity.price.toDouble(),
+              totalPrice:
+                  ProductPriceHelper.provideCurrentPrice(productEntity) *
+                  context.read<ProductQuantityCubit>().state,
+              productImage: productEntity.images[0],
+              createdDate: DateTime.now().toString(),
+            ),
+          );
+        },
         content: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
