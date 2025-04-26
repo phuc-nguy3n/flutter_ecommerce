2082f9e106b6dcbb2b6309919cd6cb6810408f40
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-26 21:21:18 +0700

    Order Repository

---
+++ lib/domain/order/repository/order.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/data/order/models/add_to_cart_req.dart';
+
+abstract class OrderRepository {
+  Future<Either> addToCart(AddToCartReq addToCartReq);
+  // Future<Either> getCartProducts();
+  // Future<Either> removeCartProduct(String id);
+  // Future<Either> orderRegistration(OrderRegistrationReq order);
+  // Future<Either> getOrders();
+}
