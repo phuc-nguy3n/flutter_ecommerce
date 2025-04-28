5be11a680732c4fdcb7c738e6b23b94f8c5f237b
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-28 21:41:44 +0700

    Cart helper

---
+++ lib/common/helper/cart/cart.dart
@@ -0,0 +1,11 @@
+import 'package:e_commerce/domain/order/entities/product_ordered.dart';
+
+class CartHelper {
+  static double calculateCartSubtotal(List<ProductOrderedEntity> products) {
+    double subtotalPrice = 0;
+    for (var item in products) {
+      subtotalPrice = subtotalPrice + item.totalPrice;
+    }
+    return subtotalPrice;
+  }
+}
