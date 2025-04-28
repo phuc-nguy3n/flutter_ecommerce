ffe6b1d940826c7f649435a515d939ecaf1b130e
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-28 21:42:50 +0700

    Data repository order repository

--- lib/data/order/repository/order.dart
+++ lib/data/order/repository/order.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/data/order/models/add_to_cart_req.dart';
+import 'package:e_commerce/data/order/models/product_ordered.dart';
 import 'package:e_commerce/data/order/source/order_firebase_service.dart';
 import 'package:e_commerce/domain/order/repository/order.dart';
 import 'package:e_commerce/service_locator.dart';
@@ -8,5 +9,35 @@ class OrderRepositoryImpl extends OrderRepository {
   @override
   Future<Either> addToCart(AddToCartReq addToCartReq) async {
     return sl<OrderFirebaseService>().addToCart(addToCartReq);
+  }
+
+  @override
+  Future<Either> getCartProducts() async {
+    var returnedData = await sl<OrderFirebaseService>().getCartProducts();
+    return returnedData.fold(
+      (error) {
+        return Left(error);
+      },
+      (data) {
+        return Right(
+          List.from(
+            data,
+          ).map((e) => ProductOrderedModel.fromMap(e).toEntity()).toList(),
+        );
+      },
+    );
+  }
+
+  @override
+  Future<Either> removeCartProduct(String id) async {
+    var returnedData = await sl<OrderFirebaseService>().removeCartProduct(id);
+    return returnedData.fold(
+      (error) {
+        return Left(error);
+      },
+      (message) {
+        return Right(message);
+      },
+    );
   }
 }
