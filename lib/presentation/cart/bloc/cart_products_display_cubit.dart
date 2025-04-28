c67a291ff425837d856a86702b729e0067b3eb6d
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-28 21:44:49 +0700

    CartProductsDisplayState

---
+++ lib/presentation/cart/bloc/cart_products_display_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:e_commerce/domain/order/entities/product_ordered.dart';
+import 'package:e_commerce/domain/order/usecases/get_cart_products.dart';
+import 'package:e_commerce/domain/order/usecases/remove_cart_product.dart';
+import 'package:e_commerce/presentation/cart/bloc/cart_products_display_state.dart';
+import 'package:e_commerce/service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CartProductsDisplayCubit extends Cubit<CartProductsDisplayState> {
+  CartProductsDisplayCubit() : super(CartProductsLoading());
+
+  void displayCartProducts() async {
+    var returnedData = await sl<GetCartProductsUseCase>().call();
+
+    returnedData.fold(
+      (error) {
+        emit(LoadCartProductsFailure(errorMessage: error));
+      },
+      (data) {
+        emit(CartProductsLoaded(products: data));
+      },
+    );
+  }
+
+  Future<void> removeProduct(ProductOrderedEntity product) async {
+    emit(CartProductsLoading());
+    var returnedData = await sl<RemoveCartProductUseCase>().call(
+      params: product.id,
+    );
+
+    returnedData.fold(
+      (error) {
+        emit(LoadCartProductsFailure(errorMessage: error));
+      },
+      (data) {
+        displayCartProducts();
+      },
+    );
+  }
+}
