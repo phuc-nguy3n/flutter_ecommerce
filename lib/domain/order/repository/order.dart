ac2760885bb6cffd866bc99b3fa0c74d0b525ccc
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-28 21:44:26 +0700

    Domain repository order

--- lib/domain/order/repository/order.dart
+++ lib/domain/order/repository/order.dart
@@ -3,8 +3,8 @@ import 'package:e_commerce/data/order/models/add_to_cart_req.dart';
 
 abstract class OrderRepository {
   Future<Either> addToCart(AddToCartReq addToCartReq);
-  // Future<Either> getCartProducts();
-  // Future<Either> removeCartProduct(String id);
+  Future<Either> getCartProducts();
+  Future<Either> removeCartProduct(String id);
   // Future<Either> orderRegistration(OrderRegistrationReq order);
   // Future<Either> getOrders();
 }
