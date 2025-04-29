6e66dba100a9db67d97fc3c21a6ac8a5e3a31768
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-29 11:34:59 +0700

    Source get orders

--- lib/data/order/source/order_firebase_service.dart
+++ lib/data/order/source/order_firebase_service.dart
@@ -9,7 +9,7 @@ abstract class OrderFirebaseService {
   Future<Either> getCartProducts();
   Future<Either> removeCartProduct(String id);
   Future<Either> orderRegistration(OrderRegistrationReq order);
-  // Future<Either> getOrders();
+  Future<Either> getOrders();
 }
 
 class OrderFirebaseServiceImpl extends OrderFirebaseService {
@@ -107,6 +107,22 @@ class OrderFirebaseServiceImpl extends OrderFirebaseService {
       }
 
       return const Right('Order registered successfully');
+    } catch (e) {
+      return const Left('Please try again');
+    }
+  }
+
+  @override
+  Future<Either> getOrders() async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+      var returnedData =
+          await FirebaseFirestore.instance
+              .collection("Users")
+              .doc(user!.uid)
+              .collection('Orders')
+              .get();
+      return Right(returnedData.docs.map((e) => e.data()).toList());
     } catch (e) {
       return const Left('Please try again');
     }
