4dc04f382acf3b74b2eb7de53eee1117836a7d96
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2025-04-29 10:20:22 +0700

    Import state favorite

--- lib/presentation/product_detail/pages/product_detail.dart
+++ lib/presentation/product_detail/pages/product_detail.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce/common/bloc/button/button_state_cubit.dart';
 import 'package:e_commerce/common/widgets/appbar/app_bar.dart';
 import 'package:e_commerce/domain/product/entities/product.dart';
+import 'package:e_commerce/presentation/product_detail/bloc/favorite_icon_cubit.dart';
 import 'package:e_commerce/presentation/product_detail/bloc/product_color_selection_cubit.dart';
 import 'package:e_commerce/presentation/product_detail/bloc/product_quantity_cubit.dart';
 import 'package:e_commerce/presentation/product_detail/bloc/product_size_selection_cubit.dart';
 import 'package:e_commerce/presentation/product_detail/widgets/add_to_bag.dart';
+import 'package:e_commerce/presentation/product_detail/widgets/favorite_button.dart';
 import 'package:e_commerce/presentation/product_detail/widgets/product_images.dart';
 import 'package:e_commerce/presentation/product_detail/widgets/product_price.dart';
 import 'package:e_commerce/presentation/product_detail/widgets/product_quantity.dart';
@@ -29,9 +31,18 @@ class ProductDetailPage extends StatelessWidget {
         BlocProvider(create: (context) => ProductSizeSelectionCubit()),
 
         BlocProvider(create: (context) => ButtonStateCubit()),
+
+        BlocProvider(
+          create:
+              (context) =>
+                  FavoriteIconCubit()..isFavorite(productEntity.productId),
+        ),
       ],
       child: Scaffold(
-        appBar: BasicAppbar(hideBack: false),
+        appBar: BasicAppbar(
+          hideBack: false,
+          action: FavoriteButton(productEntity: productEntity),
+        ),
         bottomNavigationBar: AddToBag(productEntity: productEntity),
         body: SingleChildScrollView(
           child: Column(
